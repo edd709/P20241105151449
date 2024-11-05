c011a8db8099d0ec30d897a0fb7cfebcb98fe837
Erio Daniel Díaz <erio.daniel.diaz@gmail.com>
2024-11-05 18:01:01 -0300

    commit01

---
+++ P20241105151449/P20241105151449App.swift
@@ -0,0 +1,17 @@
+//
+//  P20241105151449App.swift
+//  P20241105151449
+//
+//  Created by Erio Daniel Díaz on 5/11/24.
+//
+
+import SwiftUI
+
+@main
+struct P20241105151449App: App {
+    var body: some Scene {
+        WindowGroup {
+            View001()
+        }
+    }
+}
---
+++ P20241105151449/Views/View001.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  P20241105151449
+//
+//  Created by Erio Daniel Díaz on 5/11/24.
+//
+
+import SwiftUI
+
+struct View001: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    View001()
+}
